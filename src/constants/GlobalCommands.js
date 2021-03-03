e7facf5defcc00886b82644325e99d9577cd62f6
unb <unbelievable.tbh@gmail.com>
2021-03-03 16:36:41 +0000

    fix: include edit command in constants

--- src/constants/GlobalCommands.js
+++ src/constants/GlobalCommands.js
@@ -1,7 +1,7 @@
 //  Used to define the command structure because fs can't be used in cf workers :)
 module.exports = [
   {
-    tag: ['index', 'create', 'delete', 'raw'],
+    tag: ['index', 'create', 'edit', 'delete', 'raw'],
   },
   'say',
 ];
